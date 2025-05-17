315a9dbc52d88a10b7829b14e2f9d9c05309aba2
d0nil <moa_pochta_777@mail.ru>
2025-05-17 10:57:02 +0300

    fix

--- api-gateway/src/main/java/ru/dzrnl/apigateway/dto/AuthRequest.java
+++ api-gateway/src/main/java/ru/dzrnl/apigateway/dto/AuthRequest.java
@@ -1,8 +1,10 @@
 package ru.dzrnl.apigateway.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthRequest {
     private String login;
     private String password;
